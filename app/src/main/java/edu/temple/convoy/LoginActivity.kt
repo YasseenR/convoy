d79a8b64a6988edbb7d442187eae66085892bc08
YasseenR <114093367+YasseenR@users.noreply.github.com>
2026-02-11 17:33:37 -0500

    begin work on login activity

--- app/src/main/java/edu/temple/convoy/LoginActivity.kt
+++ app/src/main/java/edu/temple/convoy/LoginActivity.kt
@@ -3,13 +3,39 @@ package edu.temple.convoy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle? ) {
         super.onCreate(savedInstanceState)
         setContent{
-            Text("Hello World")
+            var count by remember {
+                mutableStateOf(0)
+            }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Username")
+                Text(text = count.toString())
+                Button(onClick = {
+                    count++
+                }) {
+                    Text("Login")
+                }
+            }
+
         }
     }
 }
